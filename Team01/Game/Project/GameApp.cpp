46b818efd1b6c97e308fac1efcb26f9a87c1d593
OiC-B9079-Oono <b9079@oic.jp>
2021-02-01 14:43:09 +0900

    シーンベースの追加

--- Team01/Game/Project/GameApp.cpp
+++ Team01/Game/Project/GameApp.cpp
@@ -8,16 +8,22 @@
 Mof::CTexture g_PlayerTexture;
 Mof::CTexture g_EnemyTexture;
 Character g_Player;
-CEnemy	g_Enemy;
+CEnemy*	g_Enemy = NULL;
 
 MofBool CGameApp::Initialize(void) {
     ::CUtilities::SetCurrentDirectory("Resource");
     g_PlayerTexture.Load("player/Plane1Up.png");
 	g_EnemyTexture.Load("enemy/Enemy01.png");
     g_Player.Initialize(Mof::CVector2(512.0f, 600.0f));
-	g_Enemy.Initialize(Mof::CVector2(512.0f, 100.0f));
     g_Player.SetTexture(&g_PlayerTexture);
-	g_Enemy.SetTexture(&g_EnemyTexture);
+
+	g_Enemy = new CEnemy[5];
+	float w = g_pGraphics->GetTargetWidth() / 5;
+	for (int i = 0; i < 5; i++)
+	{
+		g_Enemy[i].Initialize(Mof::CVector2(w * i, 100.0f));
+		g_Enemy[i].SetTexture(&g_EnemyTexture);
+	}
     return TRUE;
 }
 
@@ -37,7 +43,10 @@ MofBool CGameApp::Render(void) {
 
 
     g_Player.Render();
-	g_Enemy.Render();
+	for (int i = 0; i < 5; i++)
+	{
+		g_Enemy[i].Render();
+	}
 
     g_pGraphics->RenderEnd();
     return TRUE;
@@ -46,5 +55,10 @@ MofBool CGameApp::Render(void) {
 MofBool CGameApp::Release(void) {
     g_PlayerTexture.Release();
 	g_EnemyTexture.Release();
+	if (g_Enemy)
+	{
+		delete[] g_Enemy;
+		g_Enemy = NULL;
+	}
     return TRUE;
 }
