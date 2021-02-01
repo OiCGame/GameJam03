f8f3e02139b5272614973011d553833e1d72de89
mailaddressforcontact@gmail.com <mailaddressforcontact@gmail.com>
2021-02-01 18:07:55 +0900

    enemy move

--- Team01/Game/Project/Enemy.cpp
+++ Team01/Game/Project/Enemy.cpp
@@ -1,10 +1,14 @@
 #include "Enemy.h"
 
+void CEnemy::Move(void) {
+	_position.y += 5;
+}
+
 CEnemy::CEnemy() :
 	super() {
-
 }
 
 bool CEnemy::Update(std::array<CBullet, 256>& bullet_container) {
+	this->Move();
 	return true;
 }
--- Team01/Game/Project/Enemy.h
+++ Team01/Game/Project/Enemy.h
@@ -5,6 +5,12 @@
 
 class CEnemy : public CCharacter {
 	using super = CCharacter;
+private:
+	/// <summary>
+	/// ˆÚ“®
+	/// </summary>
+	/// <param name=""></param>
+	virtual void Move(void) override;
 public:
 	CEnemy();
 	virtual bool Update(std::array<CBullet, 256>& bullet_container);
